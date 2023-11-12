00aa6d5a1733f8e5988700ee7b1ef9d14eef9d2d
Nagalin punyakalin <nagalin.pun@dome.tu.ac.th>
2023-11-12 20:58:43 +0700

    create user route

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const cors = require('cors')
 require('dotenv').config()
 const app = express()
-const router = require('./routes/index')
+const router = require('./routes/')
 const PORT = process.env.PORT
 require('./database/database');
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : false}))
 app.use(cors({
     origin : 'http://localhost:3000'
 }))
