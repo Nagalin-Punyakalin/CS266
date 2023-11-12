544375cb303643123265dcbcaf2b52cb956b7da4
Nuttawut <nuttawut.ama@dome.tu.ac.th>
2023-11-12 20:16:04 +0700

    Story 1 Ui Design

--- frontend/src/feature/add-product/component/Form.tsx
+++ frontend/src/feature/add-product/component/Form.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import useAddProduct from '../hook/useAddProduct'
+import FormAddPro from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 export default function Form() {
   const {
@@ -12,10 +14,28 @@ export default function Form() {
 
   return (
     <form onSubmit={handleSubmit}>
-        <input required ref={name}  type="text" className="form-control" placeholder='Enter your product name' />
-        <input required ref={price} type="number" className="form-control" placeholder='Enter your product price'/>
-        <input required onChange={handleFileChange}  type='file'  className="form-control"/>
-        <button data-testid="submit-button"  type="submit" className="btn btn-success">Submit</button>
+        <h1>Add Product</h1>
+        <FormAddPro.Group className="ProName">
+        <FormAddPro.Label >Product Name</FormAddPro.Label>
+        <FormAddPro.Control required type="text" placeholder='Enter your Product Name' />
+        </FormAddPro.Group>
+        <p />
+        <FormAddPro.Group className="ProPrice">
+        <FormAddPro.Label>Product Price</FormAddPro.Label>
+        <FormAddPro.Control required type="number" placeholder='Enter Product Price' />
+        <FormAddPro.Text>Product Price must be number.</FormAddPro.Text>
+        </FormAddPro.Group>
+        <p />
+        <FormAddPro.Group className="ProPrice">
+        <FormAddPro.Label>Product Picture</FormAddPro.Label>
+        <FormAddPro.Control required type="file" />
+        </FormAddPro.Group>
+        <p />
+        <div className="text-center">
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+        </div>
         { error && 
           <div className="alert alert-danger" >
                 {error}
