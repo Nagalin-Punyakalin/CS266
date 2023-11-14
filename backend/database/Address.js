73d2fe0389b6ee8391a236527cab9469b73e2482
peer <peer.poc@dome.tu.ac.th>
2023-11-14 20:59:38 +0700

    changed addressInput to Address andsome minor changes

--- backend/database/Address.js
+++ backend/database/Address.js
@@ -1,9 +1,7 @@
 const mongoose = require('mongoose')
 
 const addressSchema = new mongoose.Schema({
-    realname : String,
-    address : String,
-    phone : String
+    useraddress : String
   });
 
 module.exports =  mongoose.model('User',addressSchema)
