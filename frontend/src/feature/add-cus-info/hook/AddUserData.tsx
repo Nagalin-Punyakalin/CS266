3a0cf8ab4e5634963427e8d23694693143950e3d
peer <peer.poc@dome.tu.ac.th>
2023-11-17 17:47:56 +0700

    fixing AddUserData

--- frontend/src/feature/add-cus-info/hook/AddUserData.tsx
+++ frontend/src/feature/add-cus-info/hook/AddUserData.tsx
@@ -3,38 +3,62 @@ import axios from '../../../lib/axios'
 import Swal from 'sweetalert2';
 
 export default function AddAddress() {
-    const nameRef = useRef<HTMLDivElement>(null)
-    const surnameRef = useRef<HTMLDivElement>(null)
-    const phoneRef = useRef<HTMLDivElement>(null)
-    const addressRef = useRef<HTMLDivElement>(null)
-    const emailRef = useRef<HTMLDivElement>(null)
-    const [error,setError] = useState<string>('')
+    const nameRef = useRef<HTMLInputElement>(null)
+    const surnameRef = useRef<HTMLInputElement>(null)
+    const phoneRef = useRef<HTMLInputElement>(null)
+    const houseNumberRef = useRef<HTMLInputElement>(null)
+    const villageRef = useRef<HTMLInputElement>(null)
+    const alleyRef = useRef<HTMLInputElement>(null)
+    const streetRef = useRef<HTMLInputElement>(null)
+    const subDistricRef = useRef<HTMLInputElement>(null)
+    const subAreaRef = useRef<HTMLInputElement>(null)
+    const provinceRef = useRef<HTMLInputElement>(null)
+    const postalCodeRef = useRef<HTMLInputElement>(null)
+    const emailRef = useRef<HTMLInputElement>(null)
+    const [error, setError] = useState<string>('')
 
-    const storeSubmit = () => {
+    const storeSubmit = (): void => {
         const data = {
-            nameRef: document.getElementById("formName"),
-            surnameRef: document.getElementById("formSurname"),
-            phoneRef: document.getElementById("formPhoneNumber"),
-            addressRef: document.getElementById("formAddress"),
-            emailRef: document.getElementById("formEmail"),
+            nameRef,
+            surnameRef,
+            phoneRef,
+            address: {
+                houseNumber: houseNumberRef,
+                village: villageRef,
+                alley: alleyRef,
+                street: streetRef,
+                subDistric: subDistricRef,
+                subArea: subAreaRef,
+                province: provinceRef,
+                postalCode: postalCodeRef
+            },
+            emailRef
         }
-        axios.put('/user/add-cus-info', data)
-        .then(response=>{
-            if(response.status === 201) {
-                Swal.fire(response.data.message, '', 'success')
-            }
-        })
-        .catch(err=>{
-            console.log(err.response.data.message)
-            setError(err.response.data.message)
-        })
+
+        axios.put('/user/address', data)
+            .then(response => {
+                if (response.status === 201) {
+                    Swal.fire(response.data.message, '', 'success')
+                }
+            })
+            .catch(err => {
+                console.log(err.response.data.message)
+                setError(err.response.data.message)
+            })
     }
 
     return {
         nameRef,
         surnameRef,
         phoneRef,
-        addressRef,
+        houseNumberRef,
+        villageRef,
+        alleyRef,
+        streetRef,
+        subDistricRef,
+        subAreaRef,
+        provinceRef,
+        postalCodeRef,
         emailRef,
         error,
         storeSubmit
